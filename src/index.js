3cf01b215876bc834eed4888aace2b47dfd4be70
berickal <eurasted@gmail.com>
2023-04-20 00:14:59 +0000

    My first react project

--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const DATA = [
+  { id: "todo-0", name: "Code in React", completed: true },
+  { id: "todo-1", name: "Learn c#", completed: false },
+  { id: "todo-2", name: "Push my code", completed: false }
+];
+ 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <App tasks={DATA} />
   </React.StrictMode>
 );
 
